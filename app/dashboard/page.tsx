c5bbea549081143e6483fcd6c967ca2e4978a7b6
romualdogino <romualdo.gino@gmail.com>
2024-04-05 08:04:11 -0300

    mongo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,7 +27,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      <Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
