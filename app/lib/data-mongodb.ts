ed89c7f0e52e8cde3be3eb7b1779bfc17ad015f7
romualdogino <romualdo.gino@gmail.com>
2024-04-11 17:59:56 -0300

    abriu11

--- app/lib/data-mongodb.ts
+++ app/lib/data-mongodb.ts
@@ -1,5 +1,6 @@
 'use server';
 import { PrismaClient } from '@prisma/client'
+import { Console } from 'console';
 
 const prisma = new PrismaClient()
 
@@ -12,12 +13,26 @@ export type State = {
     };
     message?: string | null;
 };
-export async function createServico(State: State , formData: FormData) {
+export async function createServico(State: State, formData: FormData) {
     // Validate form using Zod
     // const validatedFields = CreateServico.safeParse({
     //     customerId: formData.get('customerId'),
     //     amount: formData.get('amount'),
     //     status: formData.get('status'),
     // });
-    console.log(formData)
+    let nome = formData.get('nome')?.toString()
+    let tempo = formData.get('tempo')?.toString()
+    let descricao = formData.get('descricao')?.toString()
+    // console.log(formData)
+    if (nome && tempo && descricao) {
+        console.log("enviou")
+        let servico = await prisma.servico.create({
+            data: {
+                nome: nome,
+                duracao: parseInt(tempo, 10),
+                descricao: descricao
+
+            }
+        })
+    }
 }
